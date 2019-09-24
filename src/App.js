61ea6c58e1f8f7b724d573fa4e30393e16130677
iansummerlin <isummerlin95@gmail.com>
2019-09-24 20:28:16 +0100

    Set up App, Titles, Form and Weather components and openweather api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "045743a7675949345b2f05c7a53d4f9d";
+const CORS = "https://cors-anywhere.herokuapp.com/";
 
 class App extends React.Component
 {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`${CORS}http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+                          
+  }
   render()
   {
     return(
       <div>
-        <p>Hello</p>
-        <h1>World</h1>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
     );
   }
